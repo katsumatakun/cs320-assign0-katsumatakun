77d743de7600efed2df78d7c5fe2ae27f3e7e112
MasahiroKiji <mkiji@coe.edu>
2019-09-02 16:48:50 -0500

    fixed multipl.c

--- multiply.c
+++ multiply.c
@@ -9,7 +9,7 @@ void write_table(int, int, FILE*);
 int main(int argc, char* argv[]) {
 
   if (argc > 5 || argc%2 ==0) {
-    printf("usage: progam name, binary file name\n");
+    printf("usage: progam name, option, file name\n");
     return -1;
   }
   int row;
@@ -90,55 +90,6 @@ int main(int argc, char* argv[]) {
     }
   }
 
-
-/*
-  FILE *fp;
-  if((fp = fopen(argv[1], "b")) == NULL ) {
-    printf("Input file not open\n");
-    return -1;
-  }
-
-  nodePtr head_ptr = NULL;
-
-  //Allocate space for the first record while checking if there
-  //is memory space.
-  EmpPtr q;
-  q = (EmpPtr) malloc(sizeof(struct emp));
-  if(q == NULL){
-    printf("No more memory space available \n");
-    return -1;
-  }
-
-
-  while(fread(q, sizeof(struct emp), 1, fp)){
-    print_raw_data(q);
-
-    //Insert data with dainamically allocation
-    head_ptr = insertData(head_ptr, q);
-    if(head_ptr == NULL){
-      printf("No more memory space available \n");
-      return -1;
-    }
-
-    q = (EmpPtr) malloc(sizeof(struct emp));
-    if(q == NULL){
-      printf("No more memory space available \n");
-      return -1;
-    }
-}
-  free(q);  //deallocation for extra memory
-
-  if(head_ptr != NULL){
-    printf("========================================\n");
-    printf("Calculated Bi-weekly Pays in Order\n\n");
-    printf("Last name, First name  bi-weekly pay  federal tax  state tax  insurance     net pay \n");
-    print_emp_data(head_ptr);
-  }
-  else{
-    printf("No data was read\n");
-  }
-
-*/
   return 0;
 }
 
